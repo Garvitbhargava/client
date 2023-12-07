33015de08492507b71dbd60597db11d727891435
Garvitbhargava <garvitbhargava05@gmail.com>
2023-12-07 18:02:33 +0530

    update

--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -33,10 +33,32 @@ const userSlice = createSlice({
     updateUserFailure: (state, action) => {
       state.error = action.payload;
       state.loading = false;
-    }
+    },
+    deleteUserStart: (state) => {
+      state.loading = true;
+    },
+    deleteUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    deleteFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
   },
 });
 
-export const { signInStart, signInSuccess, signInFailure ,updateUserStart, updateUserFailure,updateUserSuccess } = userSlice.actions;
+export const {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+  updateUserStart,
+  updateUserFailure,
+  updateUserSuccess,
+  deleteFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+} = userSlice.actions;
 
 export default userSlice.reducer;
