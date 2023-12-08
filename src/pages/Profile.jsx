12b5c0a460cb8230652d2e715675a6f0182c2582
Garvitbhargava <garvitbhargava05@gmail.com>
2023-12-08 16:09:11 +0530

    update

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -9,6 +9,7 @@ import {
 import { app } from "../firebase.js";
 import { deleteFailure, deleteUserStart, deleteUserSuccess, signOutUserStart, signOutUserSuccess, updateUserFailure, updateUserStart, updateUserSuccess } from "../redux/user/userSlice.js";
 import { useDispatch } from "react-redux";
+import {Link} from "react-router-dom"
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -161,6 +162,7 @@ export default function Profile() {
         <button disabled={loading} className="bg-slate-700 text-white rounded-none p-3 uppercase hover:opacity-95 disabled:opacity-80 text-center">
       {loading ? "Loading..." : 'Update'}
         </button>
+        <Link className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95" to={"/create-listing"}>Create Listing</Link>
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete Account</span>
