7aa01a6c58bdc9f1c94619d14260ddc2ad3926a5
Garvitbhargava <garvitbhargava05@gmail.com>
2023-12-21 12:54:12 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Signin from "./pages/Signin";
+import Signin from "./pages/SignIn";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
